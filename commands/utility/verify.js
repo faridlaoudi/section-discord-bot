56633f079a9c516c24d7bd93387988a2abd8961b
faridlaoudi <farid.laoudi@etu.usthb.dz>
2024-02-07 23:14:00 +0100

    updating acces

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -23,24 +23,49 @@ module.exports = {
 
         const matriculeInput = interaction.options.getString('matricule').trim();
         const userIndex = userData.findIndex(u => String(u.matricule) === matriculeInput);
+		const roles = [
+			{
+				id: '1204759038026579980',
+				label: 'Section'
+			},
+			{
+				id: '1204891940718387210',
+				label: 'Group 1'
+			},
+			{
+				id: '1204892557083942963',
+				label: 'Group 2'
+			},
+			{
+				id: '1204892660012285973',
+				label: 'Group 3'
+			},
+			{
+				id: '1204893178251976725',
+				label: 'Group 4'
+			}
+		];
 
         if (userIndex !== -1) {
             const user = userData[userIndex];
             // Check if Username is already filled
             if (user.Username !== "") {
-                // Username is already set and does not match the current user's username
                 await interaction.reply('This matricule is already taken.');
             } else {
-                // Username is not set or matches the current user's username, proceed with update
                 user.Username = interaction.user.username;
                 const userInfo = `Welcome ${user.Nom} ${user.Prénom} dans Section: ${user.Section} et Groupe: ${user.Groupe}`;
                 await interaction.reply(`${userInfo}`);
+				const member = await interaction.guild.members.fetch(interaction.user.id);
+				const sectionRoleId = '1204759038026579980'; // ID for 'Section A' role
+				const groupRoleId = roles.find(role => role.label === `Group ${user.Groupe}`)?.id;
+				if (sectionRoleId) await member.roles.add(sectionRoleId);
+				if (groupRoleId) await member.roles.add(groupRoleId);
                 try {
-                    // Save the updated userData array back to the file
                     fs.writeFileSync(filePath, JSON.stringify(userData, null, 2), 'utf-8');
+                    // Assign roles based on section and group
                 } catch (error) {
-                    console.error('Error writing to the students.json file:', error);
-                    await interaction.reply('Failed to update student data. Please try again later.');
+                    console.error('Error writing to the students.json file or assigning roles:', error);
+                    await interaction.reply('Failed to update student data or assign roles. Please try again later.');
                 }
             }
         } else {
