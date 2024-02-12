7d2c2c0e82a83392d9c6789c4414fc33d0c43372
faridlaoudi <farid.laoudi@etu.usthb.dz>
2024-02-12 18:20:36 +0100

    fix

--- commands/utility/classement.js
+++ commands/utility/classement.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('deliberation')
-        .setDescription('Checks deliberation based on matricule.')
+        .setName('classement')
+        .setDescription('Checks classement based on matricule.')
         .addStringOption(option =>
             option.setName('matricule')
                 .setDescription('The matricule to check')
@@ -28,19 +28,9 @@ module.exports = {
         if (user) {
             // Extract relevant information from the user object
             const { "": name, "Moyenne du Semestre 3": moyenneSemestre, 
-            "Algèbre 3": ALGEBRE,
-            "Programmation orientée objet 1": POO,
-            "Structures de fichiers et structure de données": SFSD,
-            'Algorithmique  et complexité': ALGO,
-            "Analyse mathématique 3": ANALYSE3,
-            "Architecture  des ordinateurs 2": ARCHI,
-            "Probabilités et statistiques 2": PROBA,
-            "Entreprenariat": Entreprenariat } = user;
+            "classement S3": classement } = user;
 
-            // Check if the student passed or failed
-            const deliberation = parseFloat(moyenneSemestre) >= 10 ? 'Admis' : 'ajourne';
-
-            await interaction.reply(`Deliberation for ${name}:\nMoyenne du Semestre: ${moyenneSemestre}\nDeliberation: ${deliberation}\nAlgèbre 3: ${ALGEBRE}\nALGO: ${ALGO}\nANALYSE: ${ANALYSE3}\nPROBA: ${PROBA}\nArchi: ${ARCHI}\nSFSD: ${SFSD}\nEntreprenariat: ${Entreprenariat}\nPOO: ${POO}`);
+            await interaction.reply(`classement of ${name}:\nMoyenne du Semestre 3: ${moyenneSemestre}\nclassement: ${classement}`);
             
         } else {
             await interaction.reply('No user found with that Matricule.');
